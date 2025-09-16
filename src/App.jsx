6e4a89e63943fe7b46244373bdceffebe4c396b2
BWOH <bwoh@outlook.kr>
2025-09-16 22:54:48 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
+
+  return (
+    <div>
+      <h1>총합: {count1 + count2}</h1>
+      <Counter count={count1} onIncrement={() => setCount1(prev => prev + 1)} />
+      <Counter count={count2} onIncrement={() => setCount2(prev => prev + 1)} />
+    </div>
+  )
+}
+
+function Counter({ count, onIncrement }) {
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={onIncrement}>
+        증가
+      </button>
+    </div>
+  )
+}
+
+// function Counter(props) {
+//   return (
+//     <div>
+//       <h1>Counter: {props.count}</h1>
+//       <button onClick={props.onIncrement}>
+//         증가
+//       </button>
+//     </div>
+//   )
+// }
+
+export default App
